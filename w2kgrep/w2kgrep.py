4ca482006b509e6beafbbdc7ac39a815d4a07e50
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2022-08-29 20:31:58 -0700

    pep8 guidelines

--- w2kgrep/w2kgrep.py
+++ w2kgrep/w2kgrep.py
@@ -1,4 +1,6 @@
-import os, sys, glob
+import os
+import sys
+import glob
 
 
 def command_filter(args):
@@ -8,12 +10,12 @@ def command_filter(args):
             filtered.append(":ENE")
         elif arg in ["fermi", "eF", "FER", "EF", "ef", ":FER"]:
             filtered.append(":FER")
-        elif arg in [":MMI", "MMI", "mm", "mom", "MOM", "MM", "magmoment", "moment"]: 
+        elif arg in [":MMI", "MMI", "mm", "mom", "MOM", "MM", "magmoment", "moment"]:
             try:
                 atom = int(args[i+1])
                 atom = "00"+str(atom) if atom < 10 else "0"+str(atom)
                 filtered.append(":MMI"+atom)
-            except ValueError:
+            except Exception:
                 filtered.append(":MMI")
         else:
             filtered.append(arg)
@@ -22,9 +24,10 @@ def command_filter(args):
 
 class GREP(object):
     def __init__(self, filename=None, *args):
-        self.filename = filename if filename is not None else glob.glob("*scf")[0]
+        self.filename = filename if filename is not None else glob.glob(
+            "*scf")[0]
         self.keywords = [arg for arg in args if "-" not in arg[0]]
-        self.options  = [arg for arg in args if "-" in arg[0]]
+        self.options = [arg for arg in args if "-" in arg[0]]
         self.searches = command_filter(self.keywords)
 
     def grep(self):
@@ -38,12 +41,14 @@ class GREP(object):
             elif "-first" in self.options or "--first" in self.options:
                 print(new_grep[0])
             else:
-                for find in new_grep: print(find, end="")
+                for find in new_grep:
+                    print(find, end="")
 
 
 def main():
     orig_command_line = sys.argv[1:]
     filename = orig_command_line[-1] if "." in orig_command_line[-1] else None
-    command_line = orig_command_line[:-1] if filename is not None else orig_command_line
+    command_line = orig_command_line[:-
+                                     1] if filename is not None else orig_command_line
     grep_cli = GREP(filename, *command_line)
     grep_cli.grep()
